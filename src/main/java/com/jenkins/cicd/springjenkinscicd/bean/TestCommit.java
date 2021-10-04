0a22bca2362cb7dac5879aca9e44dc2902a83f32
ArafatGitSpace <sfbena2016@gmail.com>
2021-10-04 23:34:55 +0530

    comment added

--- src/main/java/com/jenkins/cicd/springjenkinscicd/bean/TestCommit.java
+++ src/main/java/com/jenkins/cicd/springjenkinscicd/bean/TestCommit.java
@@ -3,6 +3,6 @@ package com.jenkins.cicd.springjenkinscicd.bean;
 public class TestCommit {
 
     public static void main(String[] args) {
-        System.out.println("Hi im new commit");
+        System.out.println("Hi im new commit adding new....");
     }
 }
